727ddaf5dbe3546998a6e9caa92621d00c51cd3e
jakotr <jakub.otreba1@gmail.com>
2023-04-15 18:14:36 +0200

    add Header and LangDropdown components

---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import { useSelector } from "react-redux";
+
+//components
+import LangDropdown from "./LangDropdown";
+
+//store
+import { selectTranslation } from "../store/apps/lang";
+
+//images
+import { logo } from "../assets/index";
+
+const Header = () => {
+  //translation
+  const {
+    header: { title },
+  } = useSelector(selectTranslation);
+
+  return (
+    <header className="section rounded-none">
+      <div className="container flex justify-between items-center">
+        <span className="logo flex items-center gap-4">
+          <img src={logo} alt="Logo" width={"48px"} height={"48px"} />
+          <span>{title}</span>
+        </span>
+        <LangDropdown />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/LangDropdown.jsx
@@ -0,0 +1,24 @@
+import { useSelector, useDispatch } from "react-redux";
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
+
+//store
+import { changeLang } from "../store/apps/lang";
+
+const LangDropdown = () => {
+  const dispatch = useDispatch();
+
+  const { currentLang, languages } = useSelector((state) => state.language);
+
+  return (
+    <Dropdown
+      value={currentLang}
+      onChange={(e) => dispatch(changeLang(e.value))}
+      options={languages}
+      controlClassName="rounded-xl text-sm py-[5px] pl-2 pr-8"
+      menuClassName="text-sm"
+    />
+  );
+};
+
+export default LangDropdown;
