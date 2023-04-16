49a5f2eaefbcee1f7acca2457cf44e54b6353e1e
jakotr <jakub.otreba1@gmail.com>
2023-04-16 11:02:39 +0200

    add basic styles + dropdown dep

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,28 @@
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#2563eb", //blue-600
+        lightGray: "#e7e5e4", //stone-300
+        darkGray: "#57534e", //stone-600
+      },
+      gridTemplateColumns: {
+        list: "30px 1fr auto auto",
+        grid: "repeat(auto-fill, minmax(200px, 1fr))",
+      },
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        sm: "600px",
+        md: "728px",
+        lg: "984px",
+        xl: "1240px",
+      },
+    },
   },
   plugins: [],
-}
+};
